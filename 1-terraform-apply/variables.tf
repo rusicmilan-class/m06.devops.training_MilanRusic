ab19eaade93946bd8a30502c09e039fce2db4bb5
rusic <milan.rusic@tttech.com>
2026-05-11 10:03:01 +0200

    Change name

--- 1-terraform-apply/variables.tf
+++ 1-terraform-apply/variables.tf
@@ -23,6 +23,6 @@ variable "aws_endpoint" {
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
-  default     = "group1-Milan"
+  default     = "group1-milan"
 }
 
